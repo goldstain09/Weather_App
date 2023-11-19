afb3fddc213f3851f93001c65e900cd555ad1bdb
Sujal Rajput <rajputsujal992@gmail.com>
2023-11-19 12:09:17 +0530

    code formatting

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,44 +1,40 @@
-import React, { useState } from 'react'
-import day from '../video/video_day.mp4';
-import night from '../video/video_night.mp4';
-import './CSS/Home.scss';
-import InputAndInfo from './Input&Info';
+import React, { useState } from "react";
+import day from "../video/video_day.mp4";
+import night from "../video/video_night.mp4";
+import "./CSS/Home.scss";
+import InputAndInfo from "./Input&Info";
 
 export default function Home() {
-
-    // for only some effects
-    const [checkforday, setCheckDay] = useState(true)
-    const [checkfornight, setCheckNight] = useState(false)
-    const gettingDAYNIGHT_Value = (value) => {
-        if (value === 1) {
-            setCheckDay(true);
-            setCheckNight(false);
-        } else if (value === 0) {
-            setCheckNight(true);
-            setCheckDay(false);
-        }
+  // for only some effects
+  const [checkforday, setCheckDay] = useState(true);
+  const [checkfornight, setCheckNight] = useState(false);
+  const gettingDAYNIGHT_Value = (value) => {
+    if (value === 1) {
+      setCheckDay(true);
+      setCheckNight(false);
+    } else if (value === 0) {
+      setCheckNight(true);
+      setCheckDay(false);
     }
-    return (
-        <>
-            <div className='container-fluid'>
+  };
+  return (
+    <>
+      <div className="container-fluid">
+        {checkforday && (
+          <video autoPlay loop muted>
+            <source src={day} type="video/mp4" />
+          </video>
+        )}
+        {checkfornight && (
+          <video autoPlay loop muted>
+            <source src={night} type="video/mp4" />
+          </video>
+        )}
 
-                {
-                    checkforday && (
-                        <video autoPlay loop muted>
-                            <source src={day} type='video/mp4' />
-                        </video>) 
-                }
-                {
-                    checkfornight && (
-                        <video autoPlay loop muted>
-                            <source src={night} type='video/mp4' />
-                        </video>) 
-                }
-
-                <div className='container'>
-                    <InputAndInfo gettingDAYNIGHT_Value={gettingDAYNIGHT_Value} />
-                </div>
-            </div>
-        </>
-    )
+        <div className="container">
+          <InputAndInfo gettingDAYNIGHT_Value={gettingDAYNIGHT_Value} />
+        </div>
+      </div>
+    </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './Components/Home';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Components/Home";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Home />
